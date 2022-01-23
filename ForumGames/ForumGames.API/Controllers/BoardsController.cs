5799b11ff627b5a9f9fffa2adf3500f30c8c2529
Douglas Henrique <dev.dhbarboza@gmail.com>
2022-01-22 21:39:05 -0300

    Init Project

---
+++ ForumGames/ForumGames.API/Controllers/BoardsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumGames.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return NoContent();
+        }
+    }
+}
